7d4e988b5992350d35c781d095a37481595839d8
Daniil <159411089+Jigsaw189@users.noreply.github.com>
2026-04-15 09:22:17 +1000

    Literally changed anything IM THE FUCKING GOAT

--- NOTcheatingat-tictactoe_Daniil.py
+++ NOTcheatingat-tictactoe_Daniil.py
@@ -1,4 +1,5 @@
 win = False
+draw = False
 
 #Create Board
 board = [['□','□','□'], ['□','□','□'], ['□','□','□']]
@@ -10,9 +11,16 @@ def display_board():
 
 #Action
 def action(player):
+    display_board()
     while True:
-        down = int(input(f"Player {player}, enter down position (0-2): "))
-        across = int(input("          enter across position (0-2): "))
+        #Get Inputs
+        down = input(f"Player {player}, enter down position (0-2): ")
+        check_valid(down)
+        across = input("          enter across position (0-2): ")
+        check_valid(across)
+        down = int(down)
+        across = int(across)
+        #Check Vacancy
         if board[down][across] != "□":
             print("This spot is taken")
             continue
@@ -21,34 +29,60 @@ def action(player):
         else:
             board[down][across] = "O"
         break
+    #Check Win/Draw
     check_win(player)
+
+#Valid input
+def check_valid(a):
+    while a not in ["0", "1", "2"]:
+        if not a.isdigit():
+            display_board()
+            a = input("Please enter a number between 0 and 2: ")
+        else:
+            return a
+
+#Output
+def output(player):
+    global win
+    global winner
+    win = True
+    winner = player
 
 #Win Condition
 def check_win(player):
-    global win
-    global winner
     for i in range(3):
         #Row Check
         if board[i][0] == board[i][1] == board[i][2] == (player):
-            win = True
-            winner = player
+            output(player)
         #Column Check
         if board[0][i] == board[1][i] == board[2][i] == (player):
-            win = True
-            winner = player 
+            output(player)
     #Diagonal Check
     if board[0][0] == board[1][1] == board[2][2] == (player):
-        win = True
-        winner = player
+        output(player)
     if board[0][2] == board[1][1] == board[2][0] == (player):
-        win = True
-        winner = player
+        output(player)
+    if win == False:
+        check_draw()
+
+def check_draw():
+    global draw
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == "□":
+                return
+    draw = True
 
 #Game Loop
-while win == False:
-    display_board()
-    action("X")
-    display_board()
-    action("O")
+while not win and not draw:
+    if win == False:
+        action("X")
+    if win == False:
+        action("O")
 
-print(f"Player {winner} wins!")
+if win == True:
+    display_board()
+    print(f"Player {winner} wins!")
+if draw == True:
+    display_board()
+    print("No spaces left, Draw!")
