bf5b909d3114eb2d819ca84c8d698d7e2dc3fd52
Daniil <159411089+Jigsaw189@users.noreply.github.com>
2026-04-14 20:54:28 +1000

    I AM THE VETERAN
    Make actions def and STRUGGLING with the win condition

--- NOTcheatingat-tictactoe_Daniil.py
+++ NOTcheatingat-tictactoe_Daniil.py
@@ -1,32 +1,49 @@
+win = False
+
 #Create Board
-board = [[0,0,0], [0,0,0], [0,0,0]]
-for i in range(0,3):
-    for j in range(0,3):
-        print(board[i][j], end=" ")
-    print()
+board = [['□','□','□'], ['□','□','□'], ['□','□','□']]
+def display_board():
+    for i in range(0,3):
+        for j in range(0,3):
+            print(board[i][j], end=" ")
+        print()
 
-#Player Turns
-def player1():
+#Action
+def action(player):
     while True:
-        down = int(input("Player 1, enter down position (0-2): "))
+        down = int(input(f"Player {player}, enter down position (0-2): "))
         across = int(input("          enter across position (0-2): "))
-        if board[down][across] != 0:
+        if board[down][across] != "□":
             print("This spot is taken")
             continue
-        board[down][across] = 1
+        if player == 1:
+            board[down][across] = "X"
+        else:
+            board[down][across] = "O"
         break
 
-def player2():
-    while True:
-        down = int(input("Player 2, enter down position (0-2): "))
-        across = int(input("          enter across position (0-2): "))
-        if board[down][across] != 0:
-            print("This spot is taken")
-            continue
-        board[down][across] = 1
-        break
+#Win Condition
+def check_win():
+    for i in range (2):
+        #Row Check
+        for j in range(3):
+            if board[i][j] == board[i+1][j] and board[i][j] != "□":
+                win = True
+                return win
+        #Column Check
+        for j in range(3):
+            if board[j][i] == board[j][i+1] and board[j][i] != "□":
+                win = True
+                return win
+
 
 #Game Loop
-while True:
-    player1()
-    player2()
+while win == False:
+    display_board()
+    action(1)
+    check_win()
+    display_board()
+    action(2)
+    check_win()
+
+print("End")
