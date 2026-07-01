5de06c8824977ccbecf58337e5445b435e511a14
Christopher Parsons <chripcrackers@gmail.com>
2026-07-01 14:47:59 +1000

    Added comments into each function to further explain and clarify purpose and process

--- oop/Chri/assignments/a1/Assignment1.py
+++ oop/Chri/assignments/a1/Assignment1.py
@@ -4,20 +4,20 @@ class DeviceItem:
 
 class Laptop(DeviceItem):
     def __init__(self, laptop_id, brand, model_name):
-        super().__init__(model_name)
+        super().__init__(model_name) # To retrieve variable from parent class
         self.laptop_id = laptop_id
         self.brand = brand
-        self.available = True
+        self.available = True # By default every device is available when entered into system
 
-    def borrow_self(self):
+    def borrow_self(self): # Function to change availability when borrowed
         self.available = False
         return
 
-    def return_self(self):
+    def return_self(self): # Function to change availability when returned
         self.available = True
         return
 
-    def __str__(self):
+    def __str__(self): # Formatting for when retrieving information about laptop for human readability
         if self.available == True:
             availability = "Available"
         elif self.available == False:
@@ -33,19 +33,19 @@ class Student:
         self.name = name
         self.borrowed_laptops = []
 
-    def borrow_laptop(self, laptop_id):
+    def borrow_laptop(self, laptop_id): # Function to add a laptop into borrowed list
         self.borrowed_laptops.append(laptop_id)
         return
 
-    def return_laptop(self, laptop_id):
+    def return_laptop(self, laptop_id): # Function to removed a laptop from borrowed list
         self.borrowed_laptops.remove(laptop_id)
         return
 
-    def __str__(self):
+    def __str__(self): # Formatting for human readability on object retrieval 
         return f"{self.student_id} - {self.name}\n Borrowed laptops: {self.borrowed_laptops}"
 
 class LaptopLoanDesk:
-    def __init__(self):
+    def __init__(self): # Testing values for use in testing
         self.laptops = [
             Laptop(1, "Lenovo", "ThinkPad"),
             Laptop(2, "Dell", "Latitude"),
@@ -62,31 +62,31 @@ class LaptopLoanDesk:
             Student(5, "Ethan")
         ]
 
-    def show_laptops(self):
+    def show_laptops(self): # Retrieve list of laptops in storage
         return self.laptops
 
-    def add_laptop(self, brand, model_name):
+    def add_laptop(self, brand, model_name): # Add laptop from brand, model and an automatically assigned ID
         laptop = Laptop(len(self.laptops) + 1, brand, model_name)
         self.laptops.append(laptop)
         return laptop
 
-    def search_laptop_by_model(self, model_name):
+    def search_laptop_by_model(self, model_name): # Search laptop by matching model_name to items in the laptop list
         for laptop in self.laptops:
             if laptop.model_name == model_name:
                 result_laptop = laptop
                 return result_laptop
-        result_laptop = 0
+        result_laptop = 0 # If none are found return a null value
         return result_laptop
 
-    def show_students(self):
+    def show_students(self): # Retrieve list of students
         return self.students
 
-    def add_student(self, name):
+    def add_student(self, name): # Add student based on name, and automatically assign student ID
         student = Student(len(self.students) + 1, name)
         self.students.append(student)
         return student
 
-    def search_student_by_id(self, student_id):
+    def search_student_by_id(self, student_id): # Search for student using their unique ID
         try:
             student_id = int(student_id)
         except (TypeError, ValueError):
@@ -95,9 +95,9 @@ class LaptopLoanDesk:
         for student in self.students:
             if student.student_id == student_id:
                 return student
-        return 0
+        return 0 # If no student under that ID is found return a null value
 
-    def loan_laptop(self, student_id, laptop_id):
+    def loan_laptop(self, student_id, laptop_id): # Loan laptop by finding a student from ID and appending a specific device ID into the students personal laptops list
         try:
             student_id = int(student_id)
             laptop_id = int(laptop_id)
@@ -107,7 +107,7 @@ class LaptopLoanDesk:
 
         student = self.search_student_by_id(student_id)
         if student == 0:
-            print("Student does not exist")
+            print("Student does not exist") # Fallback method if student does not exist in system
             return None
 
         for laptop in self.laptops:
@@ -120,37 +120,36 @@ class LaptopLoanDesk:
                     print("Laptop successfully loaned!")
                     return None
 
-        print("Device ID does not exist")
+        print("Device ID does not exist") # Fallback method if no device is found
         return None
 
-    def return_laptop(self, student_id, laptop_id):
+    def return_laptop(self, student_id, laptop_id): # Return laptop by removing laptop object from the unique students personal laptops list
         try:
             student_id = int(student_id)
             laptop_id = int(laptop_id)
         except (TypeError, ValueError):
-            print("Please enter valid numeric IDs")
+            print("Please enter valid numeric IDs") # Fallback method if values are not correct type
             return None
         
         student = self.search_student_by_id(student_id)
         if student == 0:
-            print("Student does not exist")
+            print("Student does not exist") # Fallback method for when student ID does not exist
             return None
         
         for laptop in self.laptops:
             if laptop.laptop_id == laptop_id:
                 if laptop.available == True:
-                    print("ERROR: Laptop has not been loaned")
+                    print("ERROR: Laptop has not been loaned") # Fallback method for when laptop is still in storage
                 else:
                     student.return_laptop(laptop_id)
                     laptop.return_self()
                     print("Laptop successfully returned!")
                     return None        
         
-        print("Laptop successfully returned!")
         return None
 
 # Print menu and return selection from menu
-def main_menu():
+def main_menu(): # Main Menu function to quickly reprint the guide list
     print("Student Laptop Loan Management System")
     print("1. Show Laptops\
         \n2. Add laptop\
@@ -166,12 +165,12 @@ def main_menu():
 
 # Main
 
-desk = LaptopLoanDesk()
+desk = LaptopLoanDesk() # Initialise a instance of the LaptopLoanDesk class
 
 while True:
     selection = main_menu()
     
-    match selection:
+    match selection: # Method to traverse the menu and run the desired choices
         # Show laptops
         case 1:
             print("ID   Laptop Model          Availability")
@@ -193,6 +192,7 @@ while True:
             if result_laptop == 0:
                 print("Model does not exist")
             else:
+                print("Found device:")
                 print(result_laptop)
         
         # Show list of registered students
@@ -216,6 +216,7 @@ while True:
             if result_student == 0:
                 print("Student does not exist")
             else:
+                print("Found student:")
                 print(result_student)
         
         # Loan a laptop
@@ -234,3 +235,7 @@ while True:
         case 9:
             print("Goodbye!")
             break
+        
+        case _:
+            print("Choose a valid option")
+            continue
